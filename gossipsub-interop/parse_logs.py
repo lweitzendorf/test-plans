46347476bd15fdb990de582d8bf738b661ab951c
Lucas Weitzendorf <lweitzendorf@student.ethz.ch>
2025-11-27 00:31:58 +0100

    warmup time and some refactoring

--- gossipsub-interop/parse_logs.py
+++ gossipsub-interop/parse_logs.py
@@ -6,11 +6,13 @@ import shutil
 import heapq
 from collections import defaultdict
 from datetime import datetime, timedelta, timezone
-from typing import Iterable, Optional, Self
+from typing import Iterable, Optional
 
+import yaml
 import pydot
 import numpy as np
 import matplotlib.pyplot as plt
+import networkx as nx
 
 from dateutil import parser
 
@@ -23,6 +25,7 @@ bytes_sent_control: dict[str, int] = {}
 msg_send_times: dict[int, str] = {}
 msg_delivery_times: dict[int, dict[str, str]] = defaultdict(dict)
 
+
 def add_connection(topic: str, from_id: str, to_id: str) -> None:
     if topic not in meshes:
         meshes[topic] = {}
@@ -33,7 +36,6 @@ def add_connection(topic: str, from_id: str, to_id: str) -> None:
         mesh[from_id] = set()
 
     mesh[from_id].add(to_id)
-
 
 def remove_connection(topic: str, from_id: str, to_id: str) -> None:
     if not (mesh := meshes.get(topic)):
@@ -50,6 +52,20 @@ def remove_node(node_id: str):
         if node_id in mesh:
             mesh.pop(node_id)
             
+def register_sent_bytes(node_id: str, num_bytes: int, is_payload: bool) -> None:
+    bytes_sent = bytes_sent_payload if is_payload else bytes_sent_control
+    bytes_sent[node_id] = bytes_sent.get(node_id, 0) + num_bytes
+
+def register_message_send(message_id: int, node_id: str, timestamp: str) -> None:
+    if message_id not in msg_send_times:
+        msg_send_times[message_id] = timestamp
+    register_message_delivery(message_id, node_id, timestamp)
+
+def register_message_delivery(message_id: int, node_id: str, timestamp: str) -> None:
+    if (node_id not in msg_delivery_times[message_id]):
+        msg_delivery_times[message_id][node_id] = timestamp
+            
+            
 class LogEntry:
     def __init__(self, node_id: int, log: dict) -> None:
         self.node = node_id
@@ -62,41 +78,8 @@ class LogEntry:
     def __getitem__(self, arg) -> str | int:
         return self.log[arg]
     
-    def __lt__(self, other: Self) -> bool:
+    def __lt__(self, other) -> bool:
         return self.time < other.time
-
-
-def parse_log_files(root_dir: str) -> Iterable[LogEntry]:
-    root_dir = os.path.join(root_dir, "hosts")
-    
-    def parse_single_file(_node_idx: int, _file_path: str) -> Iterable[LogEntry]:
-        with open(_file_path, 'r') as f:
-            for log_line in f:
-                yield LogEntry(_node_idx, json.loads(log_line))
-                
-        # yield LogEntry(_node_idx, {"msg": "Shutdown", "time": logs[-1]["time"]})
-        
-    heads = []            
-
-    for node_dir in os.listdir(root_dir):
-        node_idx = int(node_dir.removeprefix("node"))
-            
-        for log_file in os.listdir(os.path.join(root_dir, node_dir)):
-            if not log_file.endswith(".stdout"):
-                continue
-            
-            file_path = os.path.join(root_dir, node_dir, log_file)
-            iterator = parse_single_file(node_idx, file_path)
-            if head := next(iterator, None):
-                heads.append((head, iterator))
-                
-    heapq.heapify(heads)
-
-    while heads:
-        head, iterator = heapq.heappop(heads)
-        yield head
-        if head := next(iterator, None):
-            heapq.heappush(heads, (head, iterator))
 
 
 def save_snapshot(graph_dir: str, elapsed_time: timedelta) -> None:
@@ -139,89 +122,148 @@ def save_snapshot(graph_dir: str, elapsed_time: timedelta) -> None:
             "message_sends": msg_send_times,
             "message_deliveries": msg_delivery_times
         }, f, indent=4)
+        
 
+def parse_log_files(root_dir: str) -> Iterable[LogEntry]:
+    root_dir = os.path.join(root_dir, "hosts")
+    
+    def parse_single_file(_node_idx: int, _file_path: str) -> Iterable[LogEntry]:
+        with open(_file_path, 'r') as f:
+            for log_line in f:
+                yield LogEntry(_node_idx, json.loads(log_line))
+                
+        # yield LogEntry(_node_idx, {"msg": "Shutdown", "time": logs[-1]["time"]})
+        
+    heads = []            
 
-def register_sent_bytes(node_id: str, num_bytes: int, is_payload: bool) -> None:
-    bytes_sent = bytes_sent_payload if is_payload else bytes_sent_control
-    bytes_sent[node_id] = bytes_sent.get(node_id, 0) + num_bytes
+    for node_dir in os.listdir(root_dir):
+        node_idx = int(node_dir.removeprefix("node"))
+            
+        for log_file in os.listdir(os.path.join(root_dir, node_dir)):
+            if not log_file.endswith(".stdout"):
+                continue
+            
+            file_path = os.path.join(root_dir, node_dir, log_file)
+            iterator = parse_single_file(node_idx, file_path)
+            if head := next(iterator, None):
+                heads.append((head, iterator))
+                
+    heapq.heapify(heads)
 
-def register_message_send(message_id: int, node_id: str, timestamp: str) -> None:
-    if message_id not in msg_send_times:
-        msg_send_times[message_id] = timestamp
-    register_message_delivery(message_id, node_id, timestamp)
+    while heads:
+        head, iterator = heapq.heappop(heads)
+        yield head
+        if head := next(iterator, None):
+            heapq.heappush(heads, (head, iterator))
+            
 
-def register_message_delivery(message_id: int, node_id: str, timestamp: str) -> None:
-    if (node_id not in msg_delivery_times[message_id]):
-        msg_delivery_times[message_id][node_id] = timestamp
-
-def process_logs(test_dir: str, logs: Iterable[LogEntry]) -> str:
-    snapshot_duration = timedelta(minutes=5)
+def process_logs(test_dir: str, logs: Iterable[LogEntry], warmup_time: timedelta) -> str:
+    snapshot_duration = timedelta(seconds=60)
     genesis_time = datetime(2000, 1, 1, tzinfo=timezone.utc)
-    next_snapshot = snapshot_duration
+    next_snapshot = max(warmup_time, snapshot_duration)
 
     data_dir = os.path.join(test_dir, "data")
     print(f"Creating {data_dir} ...")
     if os.path.exists(data_dir):
         shutil.rmtree(data_dir)
     os.makedirs(data_dir)
-    
-    take_snapshot = True
-    snapshot_counter = 0
 
     for log in logs:
         node_idx = log.node
+        elapsed_time = log.time - genesis_time
 
         match log["msg"]:
             case "PeerID":
                 peer_ids[node_idx] = log["id"]
             case "Sent Graft":
                 add_connection(log.get("topic", "default"), peer_ids[node_idx], log["to"])
-                register_sent_bytes(peer_ids[node_idx], log["size"], False)
-                take_snapshot = True
+                if elapsed_time >= warmup_time:
+                    register_sent_bytes(peer_ids[node_idx], log["size"], False)
             case "Received Graft":
                 add_connection(log.get("topic", "default"), peer_ids[node_idx], log["from"])
-                take_snapshot = True
             case "Added Peer":
                 add_connection("default", peer_ids[node_idx], log["id"])
-                take_snapshot = True
             case "Removed Peer":
                 remove_connection("default", peer_ids[node_idx], log["id"])
-                take_snapshot = True
             case "Sent Prune":
                 remove_connection(log.get("topic", "default"), peer_ids[node_idx], log["to"])
-                register_sent_bytes(peer_ids[node_idx], log["size"], False)
-                take_snapshot = True
+                if elapsed_time >= warmup_time:
+                    register_sent_bytes(peer_ids[node_idx], log["size"], False)
             case "Received Prune":
                 remove_connection(log.get("topic", "default"), peer_ids[node_idx], log["from"])
-                take_snapshot = True
             # case "Shutdown":
             #    remove_node(peer_ids[node_idx])
             # case "Publish":
             #    register_message_send(log["id"], peer_ids[node_idx], log["time"])
             #    register_sent_bytes(peer_ids[node_idx], log["size"], True)
             case "Sent Message":
-                register_message_send(log["id"], peer_ids[node_idx], log["time"])
-                register_sent_bytes(peer_ids[node_idx], log["size"], True)
+                if elapsed_time >= warmup_time:
+                    register_message_send(log["id"], peer_ids[node_idx], log["time"])
+                    register_sent_bytes(peer_ids[node_idx], log["size"], True)
             case "Received Message":
-                register_message_delivery(log["id"], peer_ids[node_idx], log["time"])
+                if elapsed_time >= warmup_time:
+                    register_message_delivery(log["id"], peer_ids[node_idx], log["time"])
             case msg if msg.startswith("Sent"):
-                register_sent_bytes(peer_ids[node_idx], log["size"], False)
+                if elapsed_time >= warmup_time:
+                    register_sent_bytes(peer_ids[node_idx], log["size"], False)
                 
-        elapsed_time = log.time - genesis_time
-        if take_snapshot:
-            if (snapshot_counter % 25 == 0):
-                save_snapshot(data_dir, elapsed_time)
-            snapshot_counter += 1
-            take_snapshot = False
-        elif (elapsed_time >= next_snapshot):
+        if (elapsed_time >= next_snapshot):
             save_snapshot(data_dir, elapsed_time)
             next_snapshot += snapshot_duration
-        
-
-    # end_time = datetime.fromisoformat(logs[-1]["time"])
-    # save_snapshot(data_dir, end_time - genesis_time)
 
     return data_dir
+
+
+def get_optimal_latencies(test_dir: str) -> dict[int, list[int]]:
+    graph_file_path = os.path.join(test_dir, "graph.gml")
+    G = nx.read_gml(graph_file_path, label="id")
+
+    for _, _, data in G.edges(data=True):
+        data["latency"] = int(data["latency"].removesuffix(" ms"))
+    
+    node_to_network_node = {}
+    message_sources = {}    
+
+    G_full = nx.DiGraph()
+
+    with open(os.path.join(test_dir, "shadow.yaml"), "r") as file:
+        shadow_config = yaml.safe_load(file)
+
+    for node_name, node_config in shadow_config["hosts"].items():
+        node_id = int(node_name.removeprefix("node"))
+        network_node_id = node_config["network_node_id"]
+        node_to_network_node[node_id] = network_node_id
+        G_full.add_node(node_id)
+
+    with open(os.path.join(test_dir, "params.json"), "r") as file:
+        instructions = json.load(file)
+
+    for instruction in instructions["script"]:
+        if instruction["type"] != "ifNodeIDEquals":
+            continue
+
+        node_id = instruction["nodeID"]
+        sub_instruction = instruction["instruction"]
+
+        if sub_instruction["type"] == "connect":
+            for other_node_id in sub_instruction["connectTo"]:
+                nn_1, nn_2 = node_to_network_node[node_id], node_to_network_node[other_node_id]
+                latency = nx.shortest_path_length(G, weight="latency", source=nn_1, target=nn_2)
+                if latency == 0:
+                    # same network node, but there is still latency
+                    latency = G.edges[nn_1, nn_2]["latency"]
+                G_full.add_edge(node_id, other_node_id, latency=latency)
+                G_full.add_edge(other_node_id, node_id, latency=latency)
+        elif sub_instruction["type"] == "publish":
+            message_sources[sub_instruction["messageID"]] = node_id
+            
+    optimal_latencies_ms = {}
+    for message_id, source_node_id in message_sources.items():
+        shortest_paths = nx.shortest_path_length(G_full, weight="latency", source=source_node_id)
+        shortest_paths.sort()
+        optimal_latencies_ms[message_id] = shortest_paths
+        
+    return optimal_latencies_ms
 
 
 def plot_total_network_traffic(plots_dir: str, json_data: list[tuple[int, dict]]) -> None:
@@ -241,13 +283,6 @@ def plot_total_network_traffic(plots_dir: str, json_data: list[tuple[int, dict]]
     plt.xlabel("Time (minutes)")
     plt.ylabel("Traffic multiple")
     plt.title("Multiple of Optimal Network Traffic")
-
-    # conv_factor = 1_000_000
-
-    # plt.plot(x, [e["payload"] // conv_factor for e in y], label="payload")
-    # plt.plot(x, [e["control"] // conv_factor for e in y], label="control")
-    # plt.plot(x, [e["optimal"] // conv_factor for e in y], label="payload_optimal")
-    # plt.legend()
     
     x_new = []
     y_new = []
@@ -262,7 +297,8 @@ def plot_total_network_traffic(plots_dir: str, json_data: list[tuple[int, dict]]
             
     
     # plt.ylim(bottom=1)
-    plt.plot(x_new, y_new)
+    # skip first data point to avoid large jump
+    plt.plot(x_new[1:], y_new[1:])
 
     plt.savefig(os.path.join(plots_dir, "network_traffic.png"))
     plt.clf()
@@ -271,8 +307,8 @@ def plot_total_network_traffic(plots_dir: str, json_data: list[tuple[int, dict]]
 def plot_payload_traffic_by_node(plots_dir: str, json_data: list[tuple[int, dict]]) -> None:
     node_ids = set(json_data[-1][1]["bytes_payload"].keys())
 
-    x = [0]
-    y = [{node_id: 0 for node_id in node_ids}]
+    x = []
+    y = []
 
     for total_minutes, data in json_data:
         x.append(total_minutes)
@@ -282,10 +318,8 @@ def plot_payload_traffic_by_node(plots_dir: str, json_data: list[tuple[int, dict
     plt.ylabel("Traffic (MB)")
     plt.title("Cumulative Payload Traffic by Node")
 
-    conv_factor = 1_000_000
-
     for node_id in node_ids:
-        plt.plot(x, [e[node_id] // conv_factor for e in y], label=node_id)
+        plt.plot(x, [e[node_id] // 1_000_000 for e in y], label=node_id)
 
     # plt.legend()
     plt.savefig(os.path.join(plots_dir, "payload_traffic_by_node.png"))
@@ -300,23 +334,75 @@ def plot_message_delivery_times(plots_dir: str, json_data: list[tuple[int, dict]
     plt.ylabel("Time (milliseconds)")
     plt.title("Message Delivery Latency")
 
-    x = [int(msg_id) for msg_id in msg_delivery_data.keys() if (int(msg_id) % 100 == 0)]
+    x = sorted([int(msg_id) for msg_id in msg_delivery_data.keys() if (int(msg_id) % 50 == 0)])
     y = []
-
-    conv_factor = 1000
 
     for i, msg_id in enumerate(x):
         send_ts = parser.isoparse(msg_send_data[str(msg_id)])
         delivery_times = [parser.isoparse(ts) for ts in msg_delivery_data[str(msg_id)].values()]
         delivery_times.sort()
         delivery_latencies = [ts - send_ts for ts in delivery_times[1:]]        
-        y.append([t.total_seconds() * conv_factor for t in delivery_latencies])
+        y.append([t.total_seconds() * 1000 for t in delivery_latencies])
         plt.scatter(i + np.random.normal(scale=0.1, size=len(y[-1])), y[-1], s=3)
 
     plt.boxplot(y)
     plt.xticks(list(range(len(x))), list(map(str, x)))
 
     plt.savefig(os.path.join(plots_dir, "message_latency.png"))
+    plt.clf()
+    
+    
+def plot_message_delivery_percentiles(plots_dir: str, json_data: list[tuple[int, dict]]) -> None:
+    msg_send_data = json_data[-1][1]["message_sends"]
+    msg_delivery_data = json_data[-1][1]["message_deliveries"]
+
+    plt.xlabel("Message ID")
+    plt.ylabel("Time (milliseconds)")
+    plt.title("Message Delivery Percentiles")
+
+    x = sorted([int(msg_id) for msg_id in msg_delivery_data.keys()])
+    y = {
+        20: [],
+        33: [],
+        50: [],
+        67: [],
+        80: [],
+        100: [],
+    }
+    
+    for msg_id in x:
+        send_ts = parser.isoparse(msg_send_data[str(msg_id)])
+        delivery_times = [parser.isoparse(ts) for ts in msg_delivery_data[str(msg_id)].values()]
+        delivery_times.sort()
+        delivery_latencies = [ts - send_ts for ts in delivery_times]        
+        delivery_latencies_ms = [t.total_seconds() * 1000 for t in delivery_latencies]
+        
+        if len(delivery_latencies_ms) <= 1:
+            x.remove(msg_id)
+            continue
+        
+        for percentile in y.keys():
+            latency = np.percentile(delivery_latencies_ms, percentile)
+            y[percentile].append(latency)
+
+    plt.plot(x, y[100], color="tab:green", label="P100")
+    plt.fill_between(x, y[100], color="tab:green")
+    
+    plt.plot(x, y[80], color="tab:olive", label="P80")
+    plt.fill_between(x, y[80], color="tab:olive")
+    
+    plt.plot(x, y[67], color="tab:orange", label="P67")
+    plt.fill_between(x, y[67], color="tab:orange")
+    
+    plt.plot(x, y[33], color="tab:red", label="P33")
+    plt.fill_between(x, y[33], color="tab:red")
+    
+    plt.legend()
+    
+    plt.xlim(x[0], x[-1])
+    plt.ylim(bottom=0)
+
+    plt.savefig(os.path.join(plots_dir, "message_latency_percentiles.png"))
     plt.clf()
 
 
@@ -336,11 +422,12 @@ def generate_plots(test_dir: str, data_dir: str) -> str:
         with open(json_path, "r") as f:
             json_data[total_minutes] = json.load(f)
 
-    json_data = sorted(json_data.items())
+    json_data = sorted(json_data.items())    
 
     plot_total_network_traffic(plots_dir, json_data)
     plot_payload_traffic_by_node(plots_dir, json_data)
     plot_message_delivery_times(plots_dir, json_data)
+    plot_message_delivery_percentiles(plots_dir, json_data)
 
     return plots_dir
 
@@ -352,9 +439,10 @@ def main():
     logs = parse_log_files(test_dir)
 
     print("Processing logs...")
-    data_dir = process_logs(test_dir, logs)
+    warmup_time = timedelta(minutes=150) # TODO: make configurable
+    data_dir = process_logs(test_dir, logs, warmup_time)
 
-    # data_dir = os.path.join(test_dir, "data")
+    data_dir = os.path.join(test_dir, "data")
     print("Generating graphs...")
     generate_plots(test_dir, data_dir)
     
